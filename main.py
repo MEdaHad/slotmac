f6b41c99bb2114e1317daa7b53b055200b9e7787
medahad <medahad16@gmail.com>
2022-10-15 23:41:00 +0200

    lines and deposit fnc

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from curses.ascii import isdigit
 from traceback import print_tb
+MAX_LINES = 3
 
 
 def deposit():
@@ -14,3 +15,24 @@ def deposit():
         else:
             print("Please enter a number.")
     return amount
+
+def get_number_of_lines():
+    while True:
+        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + " )? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1<= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter a Valid Number of lines.")
+        else:
+            print("Please enter a number.")
+    return lines    
+
+
+def main():
+    balance = deposit()
+    lines = get_number_of_lines()
+    print(balance, lines)
+
+main()
